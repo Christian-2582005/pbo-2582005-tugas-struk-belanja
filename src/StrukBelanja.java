428bd21ce81997e35d7826a5f7eddb9f000a0896
Christian-2582005 <2582005@unai.edu>
2026-09-09 08:06:34 +0700

    menambahkan input 5 data barang dan uang yang dibayar

---
+++ src/StrukBelanja.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class StrukBelanja {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Nama barang 1 : ");
+        String namaBarang1 = scanner.nextLine();
+        System.out.print("Harga satuan  : ");
+        int hargaBarang1 = scanner.nextInt();
+        System.out.print("Jumlah        : ");
+        int jumlahBarang1 = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.print("Nama barang 2 : ");
+        String namaBarang2 = scanner.nextLine();
+        System.out.print("Harga satuan  : ");
+        int hargaBarang2 = scanner.nextInt();
+        System.out.print("Jumlah        : ");
+        int jumlahBarang2 = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.print("Nama barang 3 : ");
+        String namaBarang3 = scanner.nextLine();
+        System.out.print("Harga satuan  : ");
+        int hargaBarang3 = scanner.nextInt();
+        System.out.print("Jumlah        : ");
+        int jumlahBarang3 = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.print("Nama barang 4 : ");
+        String namaBarang4 = scanner.nextLine();
+        System.out.print("Harga satuan  : ");
+        int hargaBarang4 = scanner.nextInt();
+        System.out.print("Jumlah        : ");
+        int jumlahBarang4 = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.print("Nama barang 5 : ");
+        String namaBarang5 = scanner.nextLine();
+        System.out.print("Harga satuan  : ");
+        int hargaBarang5 = scanner.nextInt();
+        System.out.print("Jumlah        : ");
+        int jumlahBarang5 = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.print("Uang dibayar  : ");
+        int uangDibayar = scanner.nextInt();
+
